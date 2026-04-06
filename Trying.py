32ed189294577afcad552fd51fa2fed5800ae8aa
nezzi94 <cyanish1994@gmail.com>
2026-04-06 12:35:06 -0400

    rahhhhhhh
    This program is to find the sum of a number

---
+++ Trying.py
@@ -0,0 +1,11 @@
+
+numbers= [1,2,3,4,5,6,7,8,9,10]
+
+sum_value = 5
+
+for num in(numbers):
+    sum_value*=num
+    print("the sum of all values in the list is: ",sum_value)
+
+print("the sum of all values in the list is: ",sum_value)
+
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 def main():
     print("Hello, World!")
 
-if main>3
- print(main)
